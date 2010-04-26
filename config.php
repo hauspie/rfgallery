ee634b251c4a5bfb1afc2f74ccff9a1da4ef340d
Michael Hauspie <mickey@mickey.fairy-project.org>
2010-04-26 16:54:20 +0200

    Fix js + clean config

--- config.php
+++ config.php
@@ -3,9 +3,6 @@
 $PHOTOS_DIR = "photos";
 $THUMBS_DIR = "thumbs";
 
-$URL_BASE = preg_replace("/\?.*$/", "", $_SERVER['REQUEST_URI']);
-$URL_BASE = preg_replace("/\/[^\/]*$/", "",$URL_BASE);
-
 define(HOME_PAGE_NAME, "Photos");
 
 
--- tools.php
+++ tools.php
@@ -2,6 +2,11 @@
 
 require_once("config.php");
 
+$URL_BASE = preg_replace("/\?.*$/", "", $_SERVER['REQUEST_URI']);
+$URL_BASE = preg_replace("/\/[^\/]*$/", "",$URL_BASE);
+
+
+
 function PrintHead()
 {
 ?>
@@ -9,25 +14,11 @@ function PrintHead()
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
 <head> 
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
-  <script type="text/javascript" src="yoxview/yoxview-init.js" />
+  <script type="text/javascript" src="yoxview/yoxview-init.js"></script>
   <link rel="stylesheet" href="css/style.css" type="text/css" /> 
-  <title>Photos</title>
+  <title><? echo HOME_PAGE_NAME ?></title>
   </head>
 <body>
-<div class="validator">
-<p>
-    <a href="http://validator.w3.org/check?uri=referer"><img
-        src="http://www.w3.org/Icons/valid-xhtml10"
-        alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
-</p>
-<p>
-    <a href="http://jigsaw.w3.org/css-validator/check/referer">
-        <img style="border:0;width:88px;height:31px"
-            src="http://jigsaw.w3.org/css-validator/images/vcss"
-            alt="Valid CSS!" />
-    </a>
-</p>
-</div>
 <div id='content'>
 <?
 }
